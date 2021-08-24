3900412898d31617955328d970fa443e3754a4cd
이재환 <qorvhfl080@naver.com>
2021-08-24 11:17:43 +0900

    프래그먼트 뷰페이저 배치 어댑터 작업

---
+++ app/src/main/java/com/nepplus/viewpager_20210824/adapter/MainViewPagerAdapter.kt
@@ -0,0 +1,27 @@
+package com.nepplus.viewpager_20210824.adapter
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.nepplus.viewpager_20210824.fragment.BirthFragment
+import com.nepplus.viewpager_20210824.fragment.HelloFragment
+import com.nepplus.viewpager_20210824.fragment.NameFragment
+
+class MainViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+    override fun getCount(): Int {
+//        뷰페이저가 총 몇장짜리인지 return
+        return 3
+    }
+
+    override fun getItem(position: Int): Fragment {
+//        각각의 position에 어떤 Fragment가 배치되어야 하는가
+        if (position == 0) {
+            return HelloFragment()
+        } else if (position == 1) {
+            return NameFragment()
+        } else {
+            return BirthFragment()
+        }
+    }
+}
