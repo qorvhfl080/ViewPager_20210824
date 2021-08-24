8e10f9f2dee1e8d594403d5e2539f7f102511877
이재환 <qorvhfl080@naver.com>
2021-08-24 11:05:51 +0900

    프래그먼트 3개 완성

---
+++ app/src/main/java/com/nepplus/viewpager_20210824/fragment/BirthFragment.kt
@@ -0,0 +1,32 @@
+package com.nepplus.viewpager_20210824.fragment
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.nepplus.viewpager_20210824.R
+
+class BirthFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_birth, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+    }
+
+}
